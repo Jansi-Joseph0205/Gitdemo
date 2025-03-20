c1276ce1f5a0a0d7cf660eaa8496a97e13df0d73
jansi-joseph0205 <jansivimalamary@gmail.com>
2025-03-20 15:25:37 +0530

    github integration

--- selenium/src/main/java/Jansi/PageObjectModel/CartPage.java
+++ selenium/src/main/java/Jansi/PageObjectModel/CartPage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 //import org.testng.Assert;
+//test for github integation
 
 import Jansi.AbstractClass.AbstractClass;
 
